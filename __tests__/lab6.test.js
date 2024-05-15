76cb6e93127e8497dbc1f150ddffbecfa46c0cb8
oXOjasviXo <ojasvi2003@gmail.com>
2024-05-14 21:33:59 -0700

    step 2 done

--- __tests__/lab6.test.js
+++ __tests__/lab6.test.js
@@ -30,7 +30,7 @@ describe('Basic user flow for Website', () => {
     
     // Make sure the title, price, and image are populated in the JSON
     for (let i = 0; i < prodItemsData.length; i++) {
-      console.log(`Checking product item ${i}/${prodItemsData.length}`);
+      console.log(`Checking product item ${i + 1}/${prodItemsData.length}`);
       if (prodItemsData[i].title.length == 0 || 
         prodItemsData[i].price.length == 0 || 
         prodItemsData[i].image.length == 0) {
@@ -66,9 +66,17 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the "Add to Cart" button...');
     // TODO - Step 2
     // Query a <product-item> element using puppeteer ( checkout page.$() and page.$$() in the docs )
+    let productItemHandle = await page.$('product-item');
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
+    let shadowRoot = await productItemHandle.getProperty('shadowRoot');
     // Once you have the button, you can click it and check the innerText property of the button.
+    await shadowRoot.$eval('button', button => button.click());
+    let buttonHandle = await shadowRoot.$('button');
+    let innerTxtHndl = await buttonHandle.getProperty('innerText');
     // Once you have the innerText property, use innerText.jsonValue() to get the text value of it
+    let buttonTxt = await innerTxtHndl.jsonValue();
+    //await my brain delivery
+    expect(buttonTxt).toBe('Remove from Cart');
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
